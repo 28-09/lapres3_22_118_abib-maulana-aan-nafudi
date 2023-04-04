f8b37da27dc2672b96f806716d719296e0337432
28-09 <129198598+28-09@users.noreply.github.com>
2023-04-04 21:50:56 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,7 @@
+const naviGasi = document.querySelector('.navigasi');
+// saat icon menu diklik
+document.querySelector('#icon-menu').onclick = () =>{
+    naviGasi.classList.toggle('active');
+};
+
+ 
